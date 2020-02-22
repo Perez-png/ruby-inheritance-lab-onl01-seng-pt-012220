c5156f40ae4dfbb29d0ea59f5fd6757a62de5ce0
Xavier Perez <xperez007@gmail.com>
2020-02-22 16:29:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
 class Student 
-
+  attr_accessor :first_name, :last_name
+  
+  
+  def initialize(knowledge, learn)
+    @knowledge = knowledge
+    @learn = learn 
+    @@all = self
+    
 end
